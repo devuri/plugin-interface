33616c248f834f07e0ed498ce66b271693f196c4
devuri <devuri@users.noreply.github.com>
2024-03-23 11:15:09 -0500

    test: fix tests

---
+++ tests/Unit/StarterTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+
+class StarterTest extends TestCase
+{
+    /**
+     * @param mixed $vid
+     *
+     * @throws Exception
+     */
+    public function test_fake_starter_test(): void
+    {
+        self::assertTrue(true);
+    }
+}
