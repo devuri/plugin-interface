726fbf2873ad1127ecd24edb039ee9523f64a026
devuri <devuri@users.noreply.github.com>
2024-03-23 11:18:43 -0500

    chore: fix return type

--- src/PluginInterface.php
+++ src/PluginInterface.php
@@ -31,14 +31,14 @@ interface PluginInterface
      * @param string $plugin_dir_path The directory path of the plugin. Default is an empty string.
      * @param string $plugin_dir_url  The URL of the plugin. Default is an empty string.
      *
-     * @return object An instance of the class implementing this interface.
+     * @return PluginInterface An instance of the class implementing this interface.
      *
      * Implementing classes should implement this method to ensure that the
      * plugin can be properly initialized with the specified parameters.
      * The method should return an instance of the class, ensuring that only
      * one instance of the plugin is created.
      */
-    public static function init( string $plugin_dir_path = '', string $plugin_dir_url = ''): object;
+    public static function init( string $plugin_dir_path = '', string $plugin_dir_url = ''): PluginInterface;
 
     /**
      * Hooks method.
